ff5bc4814f5a23646318d1c758d309ef29fdb238
DestructiveSoul <snehurp1703@gmail.com>
2024-08-22 16:33:06 +0530

    Update gke-ci-cd.sh

--- gke-ci-cd.sh
+++ gke-ci-cd.sh
@@ -1,6 +1,8 @@
 #Google Kubernetes Engine Pipeline using Cloud Build
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+echo $PROJECT_ID
+echo $PROJECT_NUMBER
 export REGION=us-central1
 gcloud config set compute/region $REGION
 #Run the following command to enable 
